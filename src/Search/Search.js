eb1a22231ccf208fa6c31c023da8eaaeccb475ab
Raja Rajan R <rajan170298@gmail.com>
2020-12-08 23:51:56 +0530

    Added push-to-top transition for search-bars

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Search.css';
 
-const search = () => {
-  return (
-   <div className = "Search-bar"> 
-     <input type="text" placeholder="Enter from address"></input>
-     <input type="text" placeholder="Enter to address" style={{marginLeft: '5px'}}></input>
-      <a href="#search"><span className="icon"> 
-        <svg className="_h-2 _ivP _iv1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54 54" width="54" height="54">
-          <path fill="currentColor" d="M38.25,35.59l-4.42-4.41a10.14,10.14,0,1,0-3.65,3.65l4.41,4.42a2.59,2.59,0,0,0,3.66-3.66ZM17.53,26.1a7.58,7.58,0,1,1,7.58,7.58A7.59,7.59,0,0,1,17.53,26.1Z">
-          </path>
-          </svg>
-      </span></a>
-     </div>
-  )
+class Search extends Component{
+  state = {}
+
+  svgLensIcon = "M38.25,35.59l-4.42-4.41a10.14,10.14,0,1,0-3.65,3.65l4.41,4.42a2.59,2.59,0,0,0,3.66-3.66ZM17.53,26.1a7.58,7.58,0,1,1,7.58,7.58A7.59,7.59,0,0,1,17.53,26.1Z";
+
+
+  goTop = () => {
+    this.setState({
+      marginTop: "-100px"
+    })
+  }
+
+  render(){
+    return (
+      <div id = "search-bar" className = "Search-bar-begin" style={this.state} onClick = {this.goTop}> 
+        <input type="text" placeholder="Enter from address"></input>
+        <input type="text" placeholder="Enter to address" style={{marginLeft: '5px'}}></input>
+         <a href="#search"><span className="icon"> 
+           <svg className="_h-2 _ivP _iv1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54 54" width="54" height="54">
+             <path fill="currentColor" d={this.svgLensIcon}>
+             </path>
+             </svg>
+         </span></a>
+        </div>
+     )
+  }
 }
 
-export default search;
+export default Search;
