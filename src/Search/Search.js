e0d72a0bb01c57798e09827de65a7ab6578cbbb4
Raja Rajan R <rajan170298@gmail.com>
2020-12-12 16:42:54 +0530

    Added transition for title, added quote title, added footer

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -2,20 +2,11 @@ import React, { Component } from 'react';
 import './Search.css';
 
 class Search extends Component{
-  state = {}
-
   svgLensIcon = "M38.25,35.59l-4.42-4.41a10.14,10.14,0,1,0-3.65,3.65l4.41,4.42a2.59,2.59,0,0,0,3.66-3.66ZM17.53,26.1a7.58,7.58,0,1,1,7.58,7.58A7.59,7.59,0,0,1,17.53,26.1Z";
 
-
-  goTop = () => {
-    this.setState({
-      marginTop: "-100px"
-    })
-  }
-
-  render(){
+  render(props){
     return (
-      <div id = "search-bar" className = "Search-bar-begin" style={this.state} onClick = {this.goTop}> 
+      <div id = "search-bar" className = "Search-bar" style={this.props.stateStyleChange} onClick = {this.props.pushToTop}> 
         <input type="text" placeholder="Enter from address"></input>
         <input type="text" placeholder="Enter to address" style={{marginLeft: '5px'}}></input>
          <a href="#search"><span className="icon"> 
